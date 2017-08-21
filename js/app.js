dade468cb0a18949122ebdd33f4608a4098dcd59
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-08-21 15:27:30 -0400

    added button submit function and changed placeholder text

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
+  // console.log('hi')
   var myGame = new Game();
   myGame.pickRandom();
+  // myGame.checkInput();
 
+  $('button').click(function(){
+    myGame.checkInput();
+  });
 });
--- js/count.js
+++ js/count.js
@@ -2,9 +2,20 @@ function Game () {
 
  this.availableWords = ["testing", "randomization", "music", "cat", "there", "their"];
 
+ this.strikes = 0;
+
+
+
 }
 
 Game.prototype.pickRandom = function () {
+  // console.log('hi');
   var randomWord = this.availableWords[Math.floor(Math.random() * this.availableWords.length)];
   $('.flash-card').html(randomWord);
+};
+
+Game.prototype.checkInput = function () {
+    var input = $('.text-here').val();
+    //IF THIS DO THAT :)
+
 };
