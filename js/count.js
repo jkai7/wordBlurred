249ec0aecb482e1a56f41b254d7380e379d064ef
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-08-21 16:26:59 -0400

    corrected scoping issue and function now compares lowercased version of input

--- js/count.js
+++ js/count.js
@@ -1,21 +1,23 @@
+var availableWords = ["testing", "randomization", "music", "cat", "there", "their"];
+
+var randomWord = availableWords[Math.floor(Math.random() * availableWords.length)];
+
 function Game () {
 
- this.availableWords = ["testing", "randomization", "music", "cat", "there", "their"];
-
  this.strikes = 0;
-
-
 
 }
 
 Game.prototype.pickRandom = function () {
-  // console.log('hi');
-  var randomWord = this.availableWords[Math.floor(Math.random() * this.availableWords.length)];
   $('.flash-card').html(randomWord);
 };
 
 Game.prototype.checkInput = function () {
-    var input = $('.text-here').val();
-    //IF THIS DO THAT :)
-
+    var input = $('.text-here').val().toLowerCase();
+    console.log(input);
+    if (input === randomWord){
+      console.log("yay!");
+    } else {
+      console.log("boo!");
+    }
 };
