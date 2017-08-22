8cd0b7bc7662557d38930faf7a1c1438dd8df70e
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-08-22 10:47:42 -0400

    made adjustment to var randomWord, and added setTimeout to move onto next work in both conditions, correct andas long as you have less than 3 strikes

--- js/count.js
+++ js/count.js
@@ -1,6 +1,6 @@
 var availableWords = ["testing", "randomization", "music", "cat", "there", "their", "texting", "more", "call"];
 
-var randomWord = availableWords[Math.floor(Math.random() * availableWords.length)];
+var randomWord;
 
 var strikes = 0;
 
@@ -8,31 +8,48 @@ function Game () {
 
 }
 
+
+
 Game.prototype.pickRandom = function () {
+  randomWord = availableWords[Math.floor(Math.random() * availableWords.length)];
   $('.flash-card').html(randomWord);
 };
 
 Game.prototype.checkInput = function () {
     var input = $('.text-here').val().toLowerCase();
-    this.strikes = 0;
-    //console.log(input);
+
     if (input === randomWord){
+      //DISPLAY WORD UNBLURRED
+      $('.flash-card').removeClass('blurred');
+      $('.flash-card').addClass('unblurred');
       $('.flash-card').html("Correct!");
-      $('.flash-card').removeClass('blurred');
-      $('.flash-card').addClass('unblurred');
+      // this.pickRandom();
+
+      setTimeout(this.pickRandom, 1000);
+
+      // var myThis = this;
+      // setTimeout(function () {
+      //   myThis.pickRandom();
+      // }, 1000);
+
+
+      //MOVE TO NEXT WORD
+
     } else {
-      $('.flash-card').html("Wrong!");
       strikes += 1;
-      console.log(strikes);
-      $('.x').toggle();
+      //DISPLAY WORD UNBLURRED
       $('.flash-card').removeClass('blurred');
       $('.flash-card').addClass('unblurred');
+      $('.flash-card').html("Wrong!");
+      $('.x').toggle(); //ADD ONLY ONE AT A TIME ?**maybe use prepend/append?
+      //console.log(strikes);
 
     }
+    if(strikes === 3){
+      alert("gameover!");
+    } else {
+      setTimeout(this.pickRandom, 1000);
+      //CONTINUE WITH GAME - DISPLAY NEXT WORD
+    }
+
 };
-//
-// Game.prototype.gameOver = function () {
-//   if(strikes === 3) {
-//     alert("GAME OVER");
-//   }
-// };
