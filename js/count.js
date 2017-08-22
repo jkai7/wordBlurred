2c28cb13b842422a4d48197936514437333878cb
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-08-22 16:51:41 -0400

    all basic functionality completed. Strikes are working properly. *End game bug*

--- js/count.js
+++ js/count.js
@@ -1,4 +1,7 @@
-var availableWords = ["testing", "randomization", "music", "cat", "there", "their", "texting", "more", "call"];
+var availableWords = ["testing", "where", "randomization", "music", "cat", "account",
+"ramification", "there", "their", "texting", "store", "more", "call", "effect", "affect",
+"mall", "fall", "minute", "amount", "accord", "evident", "intend", "concern", "issue", "tissue",
+"establish", "utter", "udder", "passage", "massage", "floor"];
 
 var randomWord;
 
@@ -18,15 +21,14 @@ Game.prototype.checkInput = function () {
     var input = $('.text-here').val().toLowerCase();
 
     if (input === randomWord){
-      //DISPLAY WORD UNBLURRED
+
       $('.flash-card').removeClass('blurred');
       $('.flash-card').addClass('unblurred');
       $('.flash-card').html("Correct!");
-      // this.pickRandom();
 
       setTimeout(this.pickRandom, 1500);
-      // $('.flash-card').html(randomWord).removeClass('blurred').addClass('unblurred');
       input = $('.text-here').val('');
+
       // var myThis = this;
       // setTimeout(function () {
       //   myThis.pickRandom();
@@ -35,30 +37,26 @@ Game.prototype.checkInput = function () {
 
     } else {
       strikes += 1;
-      //DISPLAY WORD UNBLURRED
+      //DISPLAY WORD UNBLURRED?
       $('.flash-card').removeClass('blurred');
       $('.flash-card').addClass('unblurred');
       $('.flash-card').html("Wrong!");
-      $('.strikes:first-child').toggle(); //ADD ONLY ONE AT A TIME ?**maybe use prepend/append?
-      //console.log(strikes);
-    //     if (strikes === 1 ) {
-    //       toggleClass(//firstone)
-    //     }
-    //     if (strikes === 2 ) {
-    //       toggleClass(//firstone)
-    //       toggleClass(//secondOne)
-    //     }
-    //
-    // }
-    if(strikes === 3){
-      $('.flash-card').html("Game Over!");
-    } else {
       setTimeout(this.pickRandom, 1500);
       // $('.flash-card').html(randomWord).removeClass('blurred').addClass('unblurred');
-
       input = $('.text-here').val('');
-      //CONTINUE WITH GAME - DISPLAY NEXT WORD
     }
 
-}
+    console.log(strikes);
+        if (strikes === 1) {
+          $('.x-1').toggle();
+        }
+        if (strikes === 2) {
+          $('.x-2').toggle();
+        }
+
+      if(strikes === 3){
+        $('.x-3').toggle();
+        $('.flash-card').html("Game Over!");
+      }
+
 };
