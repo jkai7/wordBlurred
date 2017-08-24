4ee5a29227e18ec9b8d9b9eb1ae5449ac35bcea1
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-08-24 12:38:52 -0400

    fixed styling issue, added comments

--- js/app.js
+++ js/app.js
@@ -1,26 +1,21 @@
 $(document).ready(function() {
 
+//creates new game object
   var myGame = new Game();
-  var mySound;
+//calls pickRandom prototype
   myGame.pickRandom();
 
+//calls checkInput prototype function at the press of button
   $('button').click(function(){
     myGame.checkInput();
   });
-
-
-
-//CHANGE TO JQUERY FOR CONSISTENCY
+//CHANGE TO JQUERY FOR CONSISTENCY -- allows the ability to hit "enter" to submit - also calls checkInput
   document.querySelector('input').addEventListener('keypress', function (e) {
     var key = e.which || e.keyCode;
     if (key === 13) { // 13 is enter
       myGame.checkInput();
-      // console.log("blergh");
-
     }
-
-
   });
+  //disable right click
 document.addEventListener('contextmenu', event => event.preventDefault());
-
 });
--- js/count.js
+++ js/count.js
@@ -1,4 +1,4 @@
-//NEED TO ADD MORE WORDS
+//Word array ;)
 
 var availableWords = ["testing", "where", "randomization", "music", "bologna", "fancy", "concept", "cat", "account",
 "ramification", "there", "their", "passage", "texting", "store", "more", "call", "effect", "apparent", "property", "affect",
@@ -7,52 +7,64 @@ var availableWords = ["testing", "where", "randomization", "music", "bologna", "
 "theory", "range", "confer", "conifer", "wander", "wonder", "jubilant", "exciting", "invisible", "joke", "relaxed", "daring",
 "beautiful", "daunting", "fun", "amazing", "inspire", "hack", "iron", "skill", "retro", "travel", "lazy", "jump", "mountain", "waxy",
 "pizza", "juicy", "puzzle", "muzzle", "piazza", "peep", "poop", "wallow", "willow", "noun", "noon", "pump", "divine", "gorgeous",
-"ancient", "feeling", "freeing", "code", "anxious", "unlimited", "concrete", "scribble", "marble", "flour", "swell", "abundant",
+"ancient", "feeling", "freezing", "code", "anxious", "unlimited", "concrete", "scribble", "marble", "flour", "swell", "abundant",
 "compression", "compress", "impress", "obscures", "manor", "trend", "wound", "available", ];
 
+//Game mechanic variables
 var randomWord;
-
 var strikes = 0;
-
 var score = 0;
 
+//Sound objects
 var goodJob = new Audio("./sounds/correctSound.wav");
 var noGood = new Audio("./sounds/wrong.wav");
 var gameEnd = new Audio("./sounds/gameOver.wav");
 var whoosh = new Audio("./sounds/woosh.wav");
 
-function Game () {
 
+function Game () {
 
 }
 
-
+//RANDOM WORD PICKER prototype
 Game.prototype.pickRandom = function () {
+  //Chooses a word at random from word array
   randomWord = availableWords[Math.floor(Math.random() * availableWords.length)];
+  //Removes previous "unblurred" class AND adds "blurred" class back when new random word one is chosen
   $('.flash-card').html(randomWord).addClass('blurred').removeClass('unblurred');
-  whoosh.play();
+  //Makes card pop up when new word is chosen
   $('.flash-card').removeClass('slideOutDown');
   $('.flash-card').addClass('slideInUp');
+  //"whoosh" sound that is played when card pops up
+    whoosh.play();
 };
 
-Game.prototype.checkInput = function () {
-    var input = $('.text-here').val().toLowerCase();
 
+//prototype that checks input and adds to strike/score accordingly
+Game.prototype.checkInput = function () {
+    //takes in user input and lowercases it to compare
+    var input = $('.text-here').val().toLowerCase();
+    //If input is correct conditional
     if (input === randomWord){
+      //Adds score
       score += 1;
+      //Plays positive sound
       goodJob.play();
-      console.log("your score: " + score);
+      //Removes blur from card
       $('.flash-card').removeClass('blurred');
       $('.flash-card').addClass('unblurred');
+      //Updates DOM of card to display "Correct!"
       $('.flash-card').html("Correct!");
+      //Updates DOM with your score
       $('p').html("Your score: " + score);
-
+      //Clears input in preparation of next card
       input = $('.text-here').val('');
+      //Animates the card going down
       $('.flash-card').removeClass('slideInUp');
       $('.flash-card').addClass('slideOutDown');
-      // $('.flash-card').toggleClass('slideOutDown');
+      //Delays the picking of new word
       setTimeout(this.pickRandom, 1500);
-      console.log(numCorrect);
+
 
       // var myThis = this;
       // setTimeout(function () {
